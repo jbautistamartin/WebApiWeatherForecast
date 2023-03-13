f5d70264d82754f21c046c2b8d6df7d91b9d6999
José Luis Bautista Martín <jbautista@bansi.com.mx>
2023-03-13 14:09:26 -0600

    Resolución ejemplo

---
+++ WebApiWeatherForecastClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using WebApiWeatherForecastClient.WebApiWeatherForecastServiceReference;
+
+//Establecer un certificado que no es de confianza
+var httpHandler = new HttpClientHandler();
+httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+var httpClient = new HttpClient(httpHandler);
+
+// The port number must match the port of the gRPC server.
+var client = new WebApiWeatherForecastServiceReference("https://localhost:7140", httpClient);
+
+// LLamo al servicio
+var reply = await client.GetWeatherForecastAsync();
+var strResply = JsonConvert.SerializeObject(reply, Formatting.Indented);
+
+Console.WriteLine("Result: " + strResply);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ WebApiWeatherForecastClient/WebApiWeatherForecastServiceReference.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiWeatherForecastClient.WebApiWeatherForecastServiceReference
+{
+    public static class DateOnlyExtensions
+    {
+        public static DateOnly ParseExact(string value, string dateFormat, CultureInfo invariantCulture)
+        {
+            var dateTime = DateTime.ParseExact(value, dateFormat, invariantCulture);
+
+            return new DateOnly
+            {
+                Day = dateTime.Day,
+                Month = dateTime.Month,
+                Year = dateTime.Year,
+            };
+        }
+
+        public static string ToString(this DateOnly dateOnly, string dateFormat, CultureInfo invariantCulture)
+        {
+            DateTime toString = new DateTime(dateOnly.Year, dateOnly.Month, dateOnly.Day);
+
+            return toString.ToString(dateFormat, invariantCulture);
+        }
+    }
+
+    public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+    {
+        private const string DateFormat = "yyyy-MM-dd";
+
+        public override DateOnly ReadJson(JsonReader reader, Type objectType, DateOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            return DateOnlyExtensions.ParseExact((string)reader.Value, DateFormat, CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString(DateFormat, CultureInfo.InvariantCulture));
+        }
+    }
+
+    public class TimeOnlyJsonConverter : JsonConverter<TimeOnly>
+    {
+        private const string TimeFormat = "HH:mm:ss.FFFFFFF";
+
+        public override TimeOnly ReadJson(JsonReader reader, Type objectType, TimeOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            return TimeOnly.ParseExact((string)reader.Value, TimeFormat, CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(JsonWriter writer, TimeOnly value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString(TimeFormat, CultureInfo.InvariantCulture));
+        }
+    }
+
+    public partial class WebApiWeatherForecastServiceReference
+    {
+        partial void UpdateJsonSerializerSettings(JsonSerializerSettings settings)
+
+        {
+            settings.Converters.Add(new DateOnlyJsonConverter());
+            settings.Converters.Add(new TimeOnlyJsonConverter());
+        }
+    }
+}
